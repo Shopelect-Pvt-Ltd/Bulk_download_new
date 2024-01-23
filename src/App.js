58c1e4f42db54df2d4ef1a41e22bf3890f1599cf
Punit1444 <punit@kgrp.in>
2024-01-23 20:56:26 +0530

    file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios' 
 
-function App() {
+const FileUpload = () => {
+  
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [text, setText] =  useState("")
+
+  const handleFileChange = (e) => {
+    console.log(e.target.files[0])
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleRunClick = async () => {
+    // Add your logic to run the desired functionality with the selected file
+    // For now, let's just log a message
+    const url = "http://127.0.0.1:8000/upload_run"
+    const formdata = new FormData();
+    formdata.append('file', selectedFile)
+    const config = {
+      headers:{"Accept":"application/json, text/plain, /","Content-Type": "multipart/form-data"}
+    }
+    await axios.post(url, formdata, config)
+    .then((resp)=>
+    {
+      console.log(resp)
+    }
+    )
+    .catch((e)=>{
+      console.log(e)
+    })
+    console.log('Running with file:', selectedFile);
+    setSelectedFile(null)
+    setText("File uploaded and running..")
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="file-upload-container">
+      <label htmlFor="fileInput" className="file-label">
+        Select Data File:
+      </label>
+      <input
+        type="file"
+        id="fileInput"
+        className="file-input"
+        onChange={handleFileChange}
+        accept=".csv, .txt"
+      />
+
+      <div className="selected-file">
+        {selectedFile && (
+          <div>
+            <strong>Selected File:</strong> {selectedFile.name}
+          </div>
+        )}
+      </div>
+      <div>
+        <strong>{text}</strong>
+      </div>
+      <button className="run-button" onClick={handleRunClick}>
+        Run
+      </button>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Bulk Download</h1>
+      <FileUpload />
+    </div>
+  );
+};
 
 export default App;
